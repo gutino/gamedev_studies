cea3d5b058c3d3b42a1574e857983b9b00ab6ca8
BBizarri <brunosantosbizarri@hotmail.com>
2020-10-28 22:50:36 -0300

    Tilemap, sprint, smoothmov

--- TMB_1/Coin.gd
+++ TMB_1/Coin.gd
@@ -5,7 +5,7 @@ signal pickedUp
 func _ready():
 	pass
 
-
-func _on_Coin_body_entered(_body):
-	emit_signal("pickedUp")
+func _on_Coin_body_entered(body):
+	if body.has_method("_on_Coin_pickedUp"):
+		body._on_Coin_pickedUp() 
 	self.queue_free()
