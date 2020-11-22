2b980a5aee5b1a368a6a136fc727f65ecf64f01e
Gustavo Tino <gustavotino8@gmail.com>
2020-11-21 22:16:05 -0300

    add enemy chasing behavior

--- TMB_1/Assets/Actors/Characters/OurSnek/OurSnek.gd
+++ TMB_1/Assets/Actors/Characters/OurSnek/OurSnek.gd
@@ -1,12 +1,46 @@
 extends KinematicBody2D
 
+export(int) var RightLimit := 111
+export(int) var LeftLimit := -31
+export(float) var HorizontalAccel := 5.0
+export(float) var VerticalAccel := 5.0
+
+var CurrPos := Vector2()
+var CurrVelocity := Vector2(5, 5)
+var GoingRight := true
+var PlayerPosition := Vector2()
+var PlayerNode
+
+
 func OnBodyEntered(body):
 	if body.has_method("OnDamage"):
 		body.OnDamage()
-	pass
+	
+func ChangeDirection() -> void:
+	GoingRight = !GoingRight
+	$AnimatedSprite.scale.x *= -1
+	
+	if GoingRight:
+		CurrVelocity = Vector2(HorizontalAccel, VerticalAccel)
+	else:
+		CurrVelocity = Vector2(-HorizontalAccel, VerticalAccel)
 	
 func _physics_process(delta):
-	self.move_and_slide(Vector2(5,5))
+	self.move_and_slide(CurrVelocity)
 	
+	CurrPos = get_position()
+	if CurrPos.x > RightLimit || CurrPos.x < LeftLimit:
+		ChangeDirection()
+		
+func PlayerOnArea() -> bool:
+	PlayerPosition = PlayerNode.get_position()
+	return PlayerPosition.x <= RightLimit && PlayerPosition.x >= LeftLimit
+
+func _process(delta):
+	if PlayerOnArea():
+		if ((PlayerPosition.x > CurrPos.x && !GoingRight) 
+			|| (PlayerPosition.x < CurrPos.x && GoingRight)):
+			ChangeDirection()
+			
 func _ready():
-	pass
+	PlayerNode = get_parent().get_node("OurGuy")
