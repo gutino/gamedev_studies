1118887b9278496fe36f6817b682ad9c6a013929
Gustavo Tino <gustavotino8@gmail.com>
2021-02-06 21:24:12 -0300

    fix rotation problems on cam

--- TMB_2/Assets/Objects/Actors/Misc/TDCamera/TDCamera.cs
+++ TMB_2/Assets/Objects/Actors/Misc/TDCamera/TDCamera.cs
@@ -6,15 +6,24 @@ namespace TowerDefense{
 		[Export]
 		public float CamSpeed { get; set; } = 5.0f;
 		[Export]
-		public float MouseSensitivity { get; set; } = 0.005f;
+		public float MouseSensitivity { get; set; } = 0.0025f;
 
 		private bool IsRotating { get; set; } = false;
+		private float RotationAngle;
+		private Camera Cam;
+
+		public override void _Ready(){
+			Cam = this.GetNode("Camera") as Camera;
+		}
 
 		public override void _Input(InputEvent @event){
 			if (@event is InputEventMouseMotion){
-				this.Transform = this.Transform.Rotated(Vector3.Up, MouseSensitivity * (@event as InputEventMouseMotion).Relative.x);
+				RotationAngle = -1 * MouseSensitivity * (@event as InputEventMouseMotion).Relative.x;
+				GD.Print(RotationAngle);
+				Cam.Transform = Cam.Transform.Rotated(Vector3.Up, RotationAngle);
 			}
 		}
+
 		public override void _Process(float delta){
 			if (Input.IsActionPressed("ui_left")){
 				this.Transform = this.Transform.Translated(this.Transform.basis.x * -CamSpeed * delta);
